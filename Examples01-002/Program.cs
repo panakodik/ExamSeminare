b8265397888e04c90f77d865f12e4975971651de
Aleksandr <pandom@yandex.ru>
2023-01-15 01:24:56 +0300

    Examples 01 002 Сравнение двух чисел

---
+++ Examples01-002/Program.cs
@@ -0,0 +1,14 @@
+﻿// Даны два числа. Показать большее и меньшее числа.
+Console.WriteLine("Введите последовательно два числа. Программа определит большее и меньшее из этих чисел ");
+Console.WriteLine("Введите значение первого числа ");
+double numberA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение второго числа ");
+double numberB = Convert.ToDouble(Console.ReadLine());
+if(numberA > numberB)
+{
+    Console.WriteLine($"Первое число *{numberA}* больше второго *{numberB}*");
+}
+else
+{
+    Console.WriteLine($"Второе число *{numberB}* больше первого *{numberA}*");
+}
