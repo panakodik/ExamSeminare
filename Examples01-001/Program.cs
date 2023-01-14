0891fb1c35a7429c833c7e4b3b273065090eac52
Aleksandr <pandom@yandex.ru>
2023-01-15 00:45:57 +0300

    Квадрат целого числа

---
+++ Examples01-001/Program.cs
@@ -0,0 +1,7 @@
+﻿// Вывести квадрат целого числа
+Console.WriteLine("Найти квадрат целого числа ");
+Console.WriteLine("Введите значение целого числа ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int result = numberA * numberA;
+Console.Write("Итог: ");
+Console.WriteLine(result);
